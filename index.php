cb8a2bf49daaf8b2b1423a3e12a9bcdb32ed7ae4
vantu0512 <80952782+vantu0512@users.noreply.github.com>
2021-03-23 16:32:30 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <P> <img src="sneakershop.png" />
+    </P>
+    <div id="navigation">
+        <nav id="topmenu">
+            <ul>
+              <li><a href="#">Home page</a></li>
+              <li><a href="#">Information</a></li>
+              <li><a href="#">Items</a></li>
+              <li><a href="#">Amount</a></li>
+              <li><a href="#">Other</a></li>
+            </ul>
+          </nav>
+        </div id ="header">
+    <section>
+        <article>
+            <h1> Balenciaga </h1>
+            <p>Balenciaga made in Americas 
+                (Real,Rep1:1,Fake,Super Fake)</p>
+            <img src="snaker.jpg" alt="">
+            <div class="price"> $5.000 </div>
+            <ul>
+                <li> Auto Size</li>
+                <li>Full Color</li>
+            
+            </ul>
+            <button>Mua Hàng</button>
+        </article>
+        <article>
+            <h1> Vans </h1>
+            <p> Vans made in VietNam
+                (Real,Rep1:1,Fake,Super Fake)</p>
+            <img src="vanssss.jpg" alt="">
+            <div class="price"> $3.000 </div>
+            <ul>
+                <li>Auto Size</li>
+                <li>Full Color</li>
+            </ul>
+            <button>Mua Hàng</button>
+        </article>
+        <article>
+            <h1> Converse </h1>
+            <p>Converse made in VietNam 
+                (Real,Rep1:1,Fake,Super Fake)</p>
+            <img src="giay.jpg" alt="">
+            <div class="price"> $2.000 </div>
+            <ul>
+                <li> Auto Size</li>
+                <li>Full color</li>
+        
+            
+            </ul>
+            <button>Mua Hàng</button>
+        </article>
+
+    </section>
+    
+</body>
+</html>
